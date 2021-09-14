f1c778dc5942d75cee4403a5abc2ef9bcf882da4
Fajar AM <fajar.munawar@m2.formulatrix.com>
2021-09-14 08:58:04 +0700

    initial commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,18 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { RegisterComponent } from './page/register/register.component';
+import { LoginComponent } from './page/login/login.component';
+import { DashboardComponent } from './page/dashboard/dashboard.component';
+import { ChatRoomComponent } from './page/chat-room/chat-room.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    RegisterComponent,
+    LoginComponent,
+    DashboardComponent,
+    ChatRoomComponent
   ],
   imports: [
     BrowserModule
